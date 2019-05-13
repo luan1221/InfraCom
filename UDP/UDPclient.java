5b737551750ddef3b9635ceaabfb7858717379c5
Luan Silva de Sena Advincula <lssa@ug3c19.windows.cin.ufpe.br>
2019-05-13 12:05:07 -0300

    Done UDP

---
+++ UDP/UDPclient.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class UDPclient {
+    public static void main(String[] args) throws IOException {
+        BufferedReader userMsg = new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clienteSocket = new DatagramSocket();
+        InetAddress IPserver = InetAddress.getByName("localhost");
+        byte[] sendData = new byte[2000];
+        byte[] rcvData = new byte[2000];
+        String msg = userMsg.readLine();
+        
+        sendData = msg.getBytes();
+        
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPserver, 8708);
+        long tempoIda = System.currentTimeMillis();
+        clienteSocket.send(sendPacket);
+        System.out.println(tempoIda);
+        
+        DatagramPacket resposta = new DatagramPacket(rcvData, rcvData.length);
+        clienteSocket.receive(resposta);
+        long tempoVolta = System.currentTimeMillis();
+        String r = new String(resposta.getData());
+        System.out.println(tempoVolta);
+        long rtt = tempoVolta - tempoIda;
+
+        System.out.println("Resposta recebida: " + r);
+        System.out.println("RTT: " + rtt + ".");
+        clienteSocket.close();
+        System.out.println("Encerrando a conexão...\nBye bye.");
+
+    }
+
+}    
--- UDP/UDPserver.java
+++ UDP/UDPserver.java
@@ -6,26 +6,29 @@ import java.net.InetAddress;
 public class UDPserver {
     public static void main(String[] args) throws IOException {
         DatagramSocket servidorSocket = new DatagramSocket(8708);
-        System.out.println("Estabelecido o Socket UDP servidor com número de porta 8708.");
-        System.out.println("================================================\n================================================");
+        System.out.println("Estabelecido o Socket UDP servidor com número de porta 8708.\n======================================\n======================================");
+        
         byte[] receiveData = new byte[2000];
-        byte[] sendData;
+        byte[] sendData = new byte[2000];
+        
         InetAddress clienteIP;
         int port;
+        System.out.println("Inicializando a conexão com o cliente UDP...\n======================================\n======================================");
+        
         while (true) {
-            System.out.println("Inicializando a conexão com o cliente UDP...");
-            System.out.println("================================================\n================================================");
+            
             DatagramPacket recvPacket = new DatagramPacket(receiveData, receiveData.length);
             servidorSocket.receive(recvPacket);
-            System.out.println("Pacote recebido com sucesso!");
+            System.out.println("Pacote recebido com sucesso!\n");
             clienteIP = recvPacket.getAddress();
             port = recvPacket.getPort();
-            
             
             String s = "Olá, como posso ajudá-lo?";
             sendData = s.getBytes();
             DatagramPacket send = new DatagramPacket(sendData, sendData.length, clienteIP, port);
             servidorSocket.send(send);
+            System.out.println("Respondendo ao cliente UDP...\n======================================\n======================================");
+
 
 
 
